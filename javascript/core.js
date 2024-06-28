ff803630c0e9bd6b19917de1d64ebe62585a2d92
CzupakCzupak <czupak.87u@gmail.com>
2024-06-28 11:04:58 +0200

    Changes 11:04 28.06.2024

--- javascript/core.js
+++ javascript/core.js
@@ -1,12 +1,11 @@
 const hamburgerJs = document.querySelectorAll(".hamburger-js"),
 	nav = document.querySelector(".header__nav-js"),
 	shadow = document.querySelector(".header__shadow-js"),
-	header = document.querySelector(".header-js");
-
-const hamburgerToggle = () => {
-	nav.classList.toggle("active");
-	shadow.classList.toggle("active");
-};
+	header = document.querySelector(".header-js"),
+	hamburgerToggle = () => {
+		nav.classList.toggle("active");
+		shadow.classList.toggle("active");
+	};
 
 hamburgerJs.forEach((item) => {
 	item.addEventListener("click", hamburgerToggle);
@@ -26,3 +25,24 @@ window.addEventListener("resize", () => {
 		shadow.classList.remove("active");
 	}
 });
+
+if (document.querySelectorAll("a[data-target]")) {
+	const scrollingLinks = document.querySelectorAll("a[data-target]");
+
+	scrollingLinks.forEach(function (link) {
+		link.addEventListener("click", function (e) {
+			e.preventDefault();
+			const clickedLink = e.target.closest("a");
+
+			const sectionEl = document.querySelector(`${clickedLink.dataset.target}`);
+			const topsy = sectionEl.getBoundingClientRect().top;
+			const scrollval = topsy - header.offsetHeight;
+			window.scroll(0, sectionEl);
+
+			if (nav.classList.contains("active")) {
+				nav.classList.remove("active");
+				shadow.classList.remove("active");
+			}
+		});
+	});
+}
