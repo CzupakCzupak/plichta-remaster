5473f5fc292568b5cc85a202780a2ecddb8e6a68
CzupakCzupak <czupak.87u@gmail.com>
2024-07-01 09:27:33 +0200

    Changes 09:27 01.07.2024

--- javascript/cta.js
+++ javascript/cta.js
@@ -1,8 +1,10 @@
 const label = document.querySelectorAll(".jsCtaLabel"),
-	submit = document.querySelector(".jsCtaSubmit"),
+	form = document.querySelector(".jsForm"),
 	textarea = document.querySelector(".jsCtaTextarea"),
 	phone = document.querySelector(".jsCtaPhone"),
-	email = document.querySelector(".jsCtaEmail");
+	email = document.querySelector(".jsCtaEmail"),
+	animation = document.querySelector(".jsCtaAnimation"),
+	animationOff = document.querySelector(".jsDisableAnimation");
 
 if (label) {
 	label.forEach((item) =>
@@ -24,64 +26,67 @@ if (label) {
 
 // FORM VALIDATION
 
-submit.addEventListener("click", (e) => {
-	// Mail validation
-	let errorTab = [];
+if (form) {
+	form.addEventListener("submit", (e) => {
+		// Mail validation
+		let errorTab = [];
 
-	if (
-		!String(email.value)
-			.toLowerCase()
-			.match(
-				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-			)
-	) {
-		const emailError = "Nie prawidłowy adres e-mail";
-		errorTab.push(emailError);
-	}
-
-	// Phone validation
-	if (phone.value != "") {
 		if (
-			!String(phone.value)
+			!String(email.value)
 				.toLowerCase()
-				.match(/^[0-9]{9}$/)
+				.match(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+				)
 		) {
-			const phoneError = "Nieprawidłowy numer telefonu (9 cyfr,bez przerw)";
+			const emailError = "Nie prawidłowy adres e-mail";
+			errorTab.push(emailError);
+		}
+
+		// Phone validation
+		if (phone.value != "") {
+			if (
+				!String(phone.value)
+					.toLowerCase()
+					.match(/^[0-9]{9}$/)
+			) {
+				const phoneError = "Nieprawidłowy numer telefonu (9 cyfr,bez przerw)";
+				errorTab.push(phoneError);
+			}
+		} else {
+			const phoneError = "Nieprawidłowy numer (9cyfr)";
 			errorTab.push(phoneError);
 		}
-	} else {
-		const phoneError = "Numer wymagany (9 cyfr)";
-		errorTab.push(phoneError);
-	}
 
-	// Message validation
+		// Message validation
 
-	if (textarea.value.length <= 1) {
-		const textareaError = "Podaj poprawną treść wiadomości. Co najmniej 2 znaki.";
-		errorTab.push(textareaError);
-	}
+		if (textarea.value.length <= 1) {
+			const textareaError =
+				"Podaj poprawną treść wiadomości. Co najmniej 2 znaki.";
+			errorTab.push(textareaError);
+		}
 
-	const activeCheckbox = document.querySelector(".jsCtaCheckbox.active");
-	if (!activeCheckbox) {
-		const checkboxError = "Wybierz przynajmniej jedną forme komunikacji";
-		errorTab.push(checkboxError);
-	}
+		const activeCheckbox = document.querySelector(".jsCtaCheckbox.active");
+		if (!activeCheckbox) {
+			const checkboxError = "Wybierz przynajmniej jedną z form komunikacji";
+			errorTab.push(checkboxError);
+		}
 
-	console.log(errorTab.toString());
-	if (errorTab.length != 0) {
-		window.alert(errorTab.join("\n"));
-	}
-	e.preventDefault();
+		if (errorTab.length != 0) {
+			window.alert(errorTab.join("\n"));
+		} else {
+			animation.classList.add("active");
+		}
+		e.preventDefault();
+		form.reset();
+		const activeLabels = document.querySelectorAll(".jsCtaLabel.active");
+		activeLabels.forEach((item) => {
+			item.classList.remove("active");
+			item.querySelector(".jsCtaHidden").value = "";
+			item.querySelector(".jsCtaCheckbox").classList.remove("active");
+		});
+	});
+}
+
+animationOff.addEventListener("click", () => {
+	animation.classList.remove("active");
 });
-
-// const animation = document.querySelector(".popup__container"),
-// 	popupExit = document.querySelector(".popup__exit");
-
-// popupExit.addEventListener("click", () => {
-// 	animation.classList.remove("active");
-// });
-
-// const playAnimation = () => {
-// 	animation.classList.add("active");
-// 	animation.style.maxHeight = "100vh";
-// };
